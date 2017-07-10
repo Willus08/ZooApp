902f6d089b9db15cdce3ed130b5a83ce93a0b2c5
Will <baldesw08@gmail.com>
2017-07-10 15:04:01 -0400

    updating with passing event for real this time

---
+++ app/src/main/java/com/example/android/zooatractions/PassingEvent.java
@@ -0,0 +1,18 @@
+package com.example.android.zooatractions;
+
+/**
+ * Created by Android on 6/28/2017.
+ */
+
+public class PassingEvent {
+    String passed;
+
+    public String getPassed() {
+        return passed;
+    }
+
+    public PassingEvent(String passed) {
+
+        this.passed = passed;
+    }
+}
